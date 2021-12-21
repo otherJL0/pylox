438bc3adda110ad5e24acedf5624b0063a3c7e4f
otherJL0 <jonathanglopez@gmail.com>
2021-12-21 07:49:55 -0500

    Set up basic repl

--- src/pylox/__main__.py
+++ src/pylox/__main__.py
@@ -3,12 +3,26 @@ from typing import Sequence
 
 
 def repl() -> None:
-    print("TODO: Pylox repl")
+    print("Pylox repl")
+    while True:
+        match input(">>> "):
+            case "":
+                break
+            case "exit":
+                print("Use exit() to exit")
+            case "exit()":
+                break
+            case _:
+                print("User input")
 
 
 def run_script(pylox_script: str) -> None:
-    print("TODO: Pylox run script")
-    print(f"Running {pylox_script}")
+    try:
+        with open(pylox_script, "rb") as pylox_file:
+            bytes = pylox_file.read()
+        print(bytes)
+    except FileNotFoundError:
+        print(f"No such file: {pylox_script}")
 
 
 def main(cli_args: Sequence[str]) -> None:
